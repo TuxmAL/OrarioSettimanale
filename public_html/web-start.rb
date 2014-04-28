280a0549f643de3eaa3a8544b6bb96a622816ad5
Antonio Liccardo <antonio.liccardo@unina2.it>
2014-04-28 12:59:34 +0200

    Modifica per usare come document root la directory corrente.

--- public_html/web-start.rb
+++ public_html/web-start.rb
@@ -2,7 +2,8 @@
 require 'webrick'  
 include WEBrick
 s = HTTPServer.new(:Port => 3000, :DocumentRoot    => '/')
- s.mount('/', HTTPServlet::FileHandler, '/home/tony/Progetti/OrarioSettimanale/public_html/',true)
+puts "Mounting current directory (#{Dir.pwd}) as DocumentRoot..."
+s.mount('/', HTTPServlet::FileHandler, Dir.pwd,true)
 trap 'INT' do s.shutdown end
 puts 'Hit ^C to stop serving pages.'
 s.start
